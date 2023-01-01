dffdd277b796cb67c3b1594afbbf5b946373ed51
daniyal-faisal <94481598+daniyal-faisal@users.noreply.github.com>
2023-01-01 23:02:50 +0500

    Update Searches.py

--- Searches.py
+++ Searches.py
@@ -2,10 +2,9 @@ def linearSearch(UI, lst):
     for x in range(len(lst)):
         if UI == lst[x]:
             return 1
-        else:
-            return 0
 
-def binarySearch(UI,lst):
+
+def binarySearch(UI, lst):
     UB = len(lst)
     LB = 1
     isFound = False
@@ -25,43 +24,54 @@ def binarySearch(UI,lst):
                 notinList = True
                 return 1
     except IndexError:
-            return 0
+        return 0
+
 
 # Main
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-Selection = int(input('''Please Select from the following:
+
+Selection = int(
+    input(
+        """Please Select from the following:
     1 - Linear Search
     2 - Binary Search
     0 - Exit
-Selection:'''))
+Selection:"""
+    )
+)
+
 
 CD = False
 
-while Selection != 0 :
+
+while Selection != 0:
 
     if Selection == 1:
         UI = int(input("Enter a number to be searched:"))
-        replyLS = linearSearch(UI,lst)
+        replyLS = linearSearch(UI, lst)
         if replyLS == 1:
-            print('Your number is Found')
+            print("Your number is Found")
             CD = True
         else:
-            if replyLS == 0:
-                print('Your number is NOTFound')
-                CD = True
+            print("Your number is NOTFound")
+            CD = True
     elif Selection == 2:
-            UI = int(input("Enter a number to be searched:"))
-            replyBS = binarySearch(UI, lst)
-            if replyBS == 1:
-                print('Your number is Found')
+        UI = int(input("Enter a number to be searched:"))
+        replyBS = binarySearch(UI, lst)
+        if replyBS == 1:
+            print("Your number is Found")
+            CD = True
+        else:
+            if replyBS == 0:
+                print("Your number is NOTFound")
                 CD = True
-            else:
-                if replyBS == 0:
-                    print('Your number is NOTFound')
-                    CD = True
-    print("Please select from menu only!!!")
-    Selection = int(input('''Please Select from the following:
+    Selection = int(
+        input(
+            """Please Select from the following:
         1 - Linear Search
         2 - Binary Search
-Selection:'''))
+        0 - Exit
+Selection:"""
+        )
+    )
