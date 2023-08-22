38fd75775d197c757265af2a98eddab606ef8592
Daniyal Faisal <94481598+daniyal-faisal@users.noreply.github.com>
2023-08-23 00:43:58 +0500

    Create nPr.py

---
+++ nPr.py
@@ -0,0 +1,23 @@
+def factorial(num):
+    index = 1
+    answer = 1
+
+    while (index <= num):
+
+        answer *= index
+        index += 1
+
+    return answer
+
+
+n = 0
+r= 0
+n = int(input("Enter a value for n:"))
+r = int(input("Enter a value for r:"))
+
+if(r > n or r < 0 or n <= 0):
+    print("ERROR")
+else:
+    print("nPr is",factorial(n)/(factorial(n-r)));
+
+
